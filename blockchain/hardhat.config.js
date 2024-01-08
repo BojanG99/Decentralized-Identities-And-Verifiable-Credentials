74e77a287b73d4992582153bacf245d13344011d
Bojan Galic <bojan.galic99@gmail.com>
2024-01-08 21:52:27 +0100

    Updated action steps

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks: {
+  /*  networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.API_KEY}`,
       accounts: [
-        `0x${process.env.SEPOLIA_PRIVATE_KEY_1}` /*,`0x${SEPOLIA_PRIVATE_KEY_2}`,`0x${SEPOLIA_PRIVATE_KEY_3}`*/,
+        `0x${process.env.SEPOLIA_PRIVATE_KEY_1}` ,`0x${SEPOLIA_PRIVATE_KEY_2}`,`0x${SEPOLIA_PRIVATE_KEY_3}`,
       ],
     },
-  },
+  },*/
 };
